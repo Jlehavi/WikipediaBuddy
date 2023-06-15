bf8395567ac1e12e80118b2447836f71697cb1c0
Jack Lehavi <jlehavi@gmail.com>
2023-06-15 13:47:10 -0400

    fixed message ui

--- main.py
+++ main.py
@@ -186,6 +186,7 @@ if userInput:
 #Create message system
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated']) - 1, -1, -1):
-        message(st.session_state['generated'][i], key=str(i))
         if i < len(st.session_state['past']):
             message(st.session_state['past'][i], is_user=True, key = str(i) + ' _user')
+        message(st.session_state['generated'][i], key=str(i))
+        
